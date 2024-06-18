405284100e5b99e484bfc8e210dc29443448016d
subashraidu.y <117170545+subashraidu@users.noreply.github.com>
2024-06-19 00:17:54 +0530

    Update basics.sh

--- basics.sh
+++ basics.sh
@@ -2,39 +2,28 @@
 
 #####################################################
 # version: v1
-# auther : subash
+# author : subash
 # date   : 18/6/2024
-# purpose: script to learn use of variables, arthematic functions, ifcommand, forloops, pipe and echo commands,read command.
+# purpose: script to learn use of variables, arithmetic functions, if command, for loops, pipe and echo commands, read command.
 ####################################################
-#
-echo "script to learn use of variables, arthematic functions, ifcommand, forloops, pipe and echo commands"
 
-read -p 'enter a number1 : ' num1
-read -p 'enter a number2 : ' num2
+echo "Script to learn use of variables, arithmetic functions, if command, for loops, pipe and echo commands"
 
-read -p 'enter a operator from the given options: +, _, *, %, / ' operator
+read -p 'Enter a number1 : ' num1
+read -p 'Enter a number2 : ' num2
 
-if [$operator==+]
-then 
-echo "answer = $num1+$num2"
-elif [$operator==-]
-then 
-echo "answer = $num1-$num2"
-elif [$operator==*]
-then 
-echo "answer = $num1*$num2"
-elif [$operator==%]
-then 
-echo "answer = $num1%$num2"
-elif [$operator==/]
-then 
-echo "answer = $num1/$num2"
+read -p 'Enter an operator from the given options: +, -, *, %, / ' operator
+
+if [ "$operator" == "+" ]; then
+    echo "Answer = $(($num1 + $num2))"
+elif [ "$operator" == "-" ]; then
+    echo "Answer = $(($num1 - $num2))"
+elif [ "$operator" == "*" ]; then
+    echo "Answer = $(($num1 * $num2))"
+elif [ "$operator" == "%" ]; then
+    echo "Answer = $(($num1 % $num2))"
+elif [ "$operator" == "/" ]; then
+    echo "Answer = $(($num1 / $num2))"
 else
-echo "you have entered a wrong input"
+    echo "You have entered a wrong input"
 fi
-
-
-
-
-
-
